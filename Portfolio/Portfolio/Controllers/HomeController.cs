fcd5ee20b21e93d1ad9b1c1e580536ab9ce7da82
Oskar <kogutoskar123@gmail.com>
2019-12-17 11:29:05 +0100

    Added view navigation

--- Portfolio/Portfolio/Controllers/HomeController.cs
+++ Portfolio/Portfolio/Controllers/HomeController.cs
@@ -13,17 +13,18 @@ namespace Portfolio.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Projects()
         {
-            ViewBag.Message = "Your application description page.";
+            return View();
+        }
 
+        public ActionResult CV()
+        {
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
